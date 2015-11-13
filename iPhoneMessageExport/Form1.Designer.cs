f6eedd5383efc307ede65917c520893f02f3504d
Stephen Hebert <stephenjosephhebert@gmail.com>
2015-11-13 01:26:58 -0600

    First commit

---
+++ iPhoneMessageExport/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace iPhoneMessageExport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBackups = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbMessageGroup = new System.Windows.Forms.ListBox();
+            this.labelMessageGroup = new System.Windows.Forms.Label();
+            this.labelBackupFiles = new System.Windows.Forms.Label();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBackups
+            // 
+            this.comboBackups.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBackups.Enabled = false;
+            this.comboBackups.FormattingEnabled = true;
+            this.comboBackups.ItemHeight = 13;
+            this.comboBackups.Location = new System.Drawing.Point(12, 29);
+            this.comboBackups.Name = "comboBackups";
+            this.comboBackups.Size = new System.Drawing.Size(586, 21);
+            this.comboBackups.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbMessageGroup);
+            this.panel1.Controls.Add(this.labelMessageGroup);
+            this.panel1.Controls.Add(this.labelBackupFiles);
+            this.panel1.Controls.Add(this.btnLoad);
+            this.panel1.Controls.Add(this.btnExport);
+            this.panel1.Controls.Add(this.comboBackups);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(645, 359);
+            this.panel1.TabIndex = 4;
+            // 
+            // lbMessageGroup
+            // 
+            this.lbMessageGroup.FormattingEnabled = true;
+            this.lbMessageGroup.Location = new System.Drawing.Point(12, 78);
+            this.lbMessageGroup.Name = "lbMessageGroup";
+            this.lbMessageGroup.Size = new System.Drawing.Size(621, 238);
+            this.lbMessageGroup.TabIndex = 7;
+            this.lbMessageGroup.SelectedIndexChanged += new System.EventHandler(this.lbMessageGroup_SelectedIndexChanged);
+            // 
+            // labelMessageGroup
+            // 
+            this.labelMessageGroup.AutoSize = true;
+            this.labelMessageGroup.Location = new System.Drawing.Point(12, 62);
+            this.labelMessageGroup.Name = "labelMessageGroup";
+            this.labelMessageGroup.Size = new System.Drawing.Size(104, 13);
+            this.labelMessageGroup.TabIndex = 6;
+            this.labelMessageGroup.Text = "Message Group List:";
+            // 
+            // labelBackupFiles
+            // 
+            this.labelBackupFiles.AutoSize = true;
+            this.labelBackupFiles.Location = new System.Drawing.Point(12, 13);
+            this.labelBackupFiles.Name = "labelBackupFiles";
+            this.labelBackupFiles.Size = new System.Drawing.Size(71, 13);
+            this.labelBackupFiles.TabIndex = 5;
+            this.labelBackupFiles.Text = "Backup Files:";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Image = ((System.Drawing.Image)(resources.GetObject("btnLoad.Image")));
+            this.btnLoad.Location = new System.Drawing.Point(604, 24);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(29, 29);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Enabled = false;
+            this.btnExport.Location = new System.Drawing.Point(12, 324);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(621, 23);
+            this.btnExport.TabIndex = 0;
+            this.btnExport.Text = "Export Messages for Selected Message Group to HTML";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 359);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "iPhone Message Exporter";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBackups;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label labelBackupFiles;
+        private System.Windows.Forms.ListBox lbMessageGroup;
+        private System.Windows.Forms.Label labelMessageGroup;
+    }
+}
+
---
+++ iPhoneMessageExport/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
+
+
+namespace iPhoneMessageExport
+{
+
+    public partial class Form1 : Form
+    {
+
+        public static int getTimestamp(DateTime datetime)
+        {
+            DateTime sTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            return (int)(datetime - sTime).TotalSeconds;
+        }
+
+        public static DateTime timestampToDateTime(int timestamp)
+        {
+            DateTime sTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return sTime.AddSeconds(timestamp);
+        }
+
+        private string addHTMLHeaders ( string html )
+        {
+            html += "@import url(https://fonts.googleapis.com/css?family=Lato|Lato:300);\n";
+            html += "\n";
+            html += "body {\n";
+            html += "	margin: 0 auto;\n";
+            html += "	padding: 1.5em 0;\n";
+            html += "	width: 640px;\n";
+            html += "	font-family: \"Georgia\";\n";
+            html += "}\n";
+            html += "\n";
+            html += "h1 {\n";
+            html += "	margin-bottom: 0px;\n";
+            html += "}\n";
+            html += "\n";
+            html += "h2 {\n";
+            html += "	margin-top: 0px;\n";
+            html += "	font-size: 10pt;\n";
+            html += "}\n";
+            html += "\n";
+            html += "#messages {\n";
+            html += "	font-family: \"HelveticaNeue-Light\", \"Helvetica Neue Light\", \"Helvetica Neue\", \"Lato\", Helvetica, Arial, \"Lucida Grande\", sans-serif; \n";
+            html += "	font-weight: 400;\n";
+            html += "   overflow: auto;\n";
+            html += "}\n";
+            html += "\n";
+            html += "#messages .content {\n";
+            html += "   clear: left;\n";
+            html += "	padding: 15px 20px 15px 20px;\n";
+            html += "	border-radius: 1.5em;\n";
+            html += "	max-width: 320px;\n";
+            html += "	font-size: 16pt;\n";
+            html += "	margin: 3px;\n";
+            html += "   word-break:break-word;\n";
+            html += "}\n";
+            html += "\n";
+            html += "#messages img {\n";
+            html += "	max-width: 320px;\n";
+            html += "	border-bottom: 3px solid #fff;\n";
+            html += "}\n";
+            html += "\n";
+            html += "#messages img:first-child {\n";
+            html += "	border-top-left-radius: 1em;\n";
+            html += "	border-top-right-radius: 1em;\n";
+            html += "}\n";
+            html += "\n";
+            html += "#messages img:last-child {\n";
+            html += "   border: 0;\n";
+            html += "	border-bottom-left-radius: 1em;\n";
+            html += "	border-bottom-right-radius: 1em;\n";
+            html += "}\n";
+            html += "\n";
+            html += "#messages .sender {\n";
+            html += "	clear: left;\n";
+            html += "	float: left;\n";
+            html += "	margin-top: 5px;\n";
+            html += "	margin-left: 10px;\n";
+            html += "	font-size: 8pt;\n";
+            html += "}\n";
+            html += "\n";
+            html += "#messages .timestamp {\n";
+            html += "	clear: both;\n";
+            html += "	float: right;\n";
+            html += "	padding: 10px 15px 10px 15px;\n";
+            html += "	width: 80px;\n";
+            html += "	font-size: 8pt;\n";
+            html += "	text-align: center;\n";
+            html += "	color: #8E8E93;\n";
+            html += "}\n";
+            html += "\n";
+            html += "#messages .message-RCVD-iMessage .content, #messages .message-RCVD-SMS .content {\n";
+            html += "	background-color: #e5e5ea;\n";
+            html += "	float: left;\n";
+            html += "}\n";
+            html += "\n";
+            html += "#messages .message-SENT-SMS .content {\n";
+            html += "	background-color: #00dd4c;\n";
+            html += "	color: #fff;\n";
+            html += "	float: right;\n";
+            html += "}\n";
+            html += "\n";
+            html += "#messages .message-SENT-iMessage .content {\n";
+            html += "	background-color: #188dfd;\n";
+            html += "	color: #fff;\n";
+            html += "	float: right;\n";
+            html += "}\n";
+            return html;
+        }
+
+        DataTable dtMessageFiles;
+        string dbFile = null;
+        string dbFileDate = null;
+        string dbFileDir = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string backupPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Apple Computer\MobileSync\Backup";
+            DirectoryInfo dirBackup = new DirectoryInfo(backupPath);
+            if (!dirBackup.Exists)
+            {
+                MessageBox.Show("Error: Cannot find iPhone backup folder.");
+                return; // the path does not exist
+            }
+
+            dtMessageFiles = new DataTable();
+            dtMessageFiles.Columns.Add("Timestamp", typeof(int));
+            dtMessageFiles.Columns.Add("Path", typeof(string));
+            dtMessageFiles.Columns.Add("FullDate", typeof(string));
+            dtMessageFiles.DefaultView.Sort = "Timestamp DESC";
+
+            DirectoryInfo[] dirBackups = dirBackup.GetDirectories("*.", SearchOption.TopDirectoryOnly);
+            FileInfo[] files = null;
+            foreach (DirectoryInfo dir in dirBackups)
+            {
+                // check that it contains the messages file (3d0d7e5fb2ce288813306e4d4636395e047a3d28)
+                files = dir.GetFiles("3d0d7e5fb2ce288813306e4d4636395e047a3d28", SearchOption.TopDirectoryOnly);
+                if (files != null)
+                {
+                    foreach (System.IO.FileInfo fi in files)
+                    {
+                        // Unix Timestamp will overflow in the year 2038
+                        dtMessageFiles.Rows.Add(getTimestamp(fi.CreationTime), fi.FullName, fi.CreationTime.ToString("f"));
+                    }
+                }
+
+            }
+
+            DataView dvMessageFiles = dtMessageFiles.DefaultView;
+            comboBackups.DataSource = new BindingSource(dvMessageFiles, null);
+            comboBackups.DisplayMember = "FullDate";
+            comboBackups.ValueMember = "Path";
+
+            // enable comboBackups
+            comboBackups.Enabled = true;
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            // save selected dbFile for later use
+            dbFile = comboBackups.SelectedValue.ToString();
+            dbFileDate = comboBackups.GetItemText(comboBackups.SelectedItem);
+            dbFileDir = dbFile.Substring(0,dbFile.LastIndexOf(@"\"));
+
+            // open SQLite data file
+            SQLiteConnection m_dbConnection;
+            m_dbConnection = new SQLiteConnection("Data Source="+dbFile+";Version=3;Read Only=True;FailIfMissing=True;");
+            m_dbConnection.Open();
+
+            // add error handling
+
+            // ### select the data
+            string sql = "SELECT DISTINCT (SELECT GROUP_CONCAT(h.id) FROM chat_handle_join ch INNER JOIN handle h on h.ROWID = ch.handle_id " + 
+                "WHERE ch.chat_id = cm.chat_id GROUP BY ch.chat_id) as chatgroup FROM chat_message_join cm " + 
+                "INNER JOIN message m ON cm.message_id = m.ROWID INNER JOIN handle h ON m.handle_id = h.ROWID ORDER BY chatgroup;";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader row = command.ExecuteReader();
+            lbMessageGroup.Items.Clear();
+            while (row.Read())
+            {
+                //Console.WriteLine("Chatgroup: " + row["chatgroup"]);
+                if (row["chatgroup"].ToString().Trim()!="")
+                    lbMessageGroup.Items.Add(row["chatgroup"]);
+            }
+            //Console.ReadKey();
+            row.Close();
+            m_dbConnection.Close(); // should only do this once?
+        }
+
+        private void lbMessageGroup_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnExport.Enabled = true;
+        }
+
+        public static string HexStringFromBytes(byte[] bytes)
+        {
+            var sb = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                var hex = b.ToString("x2");
+                sb.Append(hex);
+            }
+            return sb.ToString();
+        }
+
+        private static string getSHAHash(string s)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(s);
+            using (var sha1 = SHA1.Create())
+            {
+                byte[] hashBytes = sha1.ComputeHash(bytes);
+                return HexStringFromBytes(hashBytes);
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            string chatGroup = lbMessageGroup.GetItemText(lbMessageGroup.SelectedItem);
+            bool isGroupMessage = (chatGroup.Contains(",")) ? true : false;
+            string htmlOutput = "";
+
+            // FEATURE: HTML PREVIEW
+
+            // show dialog for where to save html file
+            SaveFileDialog htmlFileDialog = new SaveFileDialog();
+            htmlFileDialog.Filter = "HTML File|*.htm,*.html";
+            htmlFileDialog.Title = "Save an HTML File";
+            htmlFileDialog.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (htmlFileDialog.FileName == "")
+            {
+                //MessageBox.Show("Filename cannot be empty!");
+                return;
+            }
+
+            //MessageBox.Show(htmlFileDialog.FileName);
+
+            // query database
+            if (dbFile!=null)
+            {
+                // open SQLite data file
+                SQLiteConnection m_dbConnection;
+                m_dbConnection = new SQLiteConnection("Data Source=" + dbFile + ";Version=3;Read Only=True;FailIfMissing=True;");
+                m_dbConnection.Open();
+
+                // add error handling
+
+                // ### select the data
+                string sql = "SELECT cm.chat_id, (SELECT GROUP_CONCAT(h.id) FROM chat_handle_join ch INNER JOIN handle h on h.ROWID = ch.handle_id " +
+                    "WHERE ch.chat_id = cm.chat_id GROUP BY ch.chat_id) as chatgroup, datetime(m.date+978307200,\"unixepoch\",\"localtime\") as date, " +
+                    "m.service, CASE m.is_from_me WHEN 1 THEN \"SENT\" WHEN 0 THEN \"RCVD\" END as direction, h.id, " + 
+                    "CASE m.type WHEN 1 THEN \"GROUP\" WHEN 0 THEN \"1 - ON - 1\" END as type, replace(m.text,cast(X'EFBFBC' as text),\"[MEDIA]\") as text, " +
+                    "(SELECT GROUP_CONCAT(\"MediaDomain-\"||substr(a.filename,3)) FROM message_attachment_join ma " +
+                    "JOIN attachment a ON ma.attachment_id = a.ROWID WHERE ma.message_id = m.ROWID GROUP BY ma.message_id) as filereflist " +
+                    "FROM chat_message_join cm INNER JOIN message m ON cm.message_id = m.ROWID INNER JOIN handle h ON m.handle_id = h.ROWID " +
+                    "WHERE chatgroup = \""+chatGroup+"\" ORDER BY date";
+                //string sql = "SELECT m.ROWID, cm.chat_id, (SELECT GROUP_CONCAT(h.id) FROM chat_handle_join ch INNER JOIN handle h on h.ROWID = ch.handle_id WHERE ch.chat_id = cm.chat_id GROUP BY ch.chat_id) as chatgroup, datetime(m.date+978307200,\"unixepoch\",\"localtime\") as date, m.service, CASE m.is_from_me WHEN 1 THEN \"SENT\" WHEN 0 THEN \"RCVD\" END as direction, h.id, CASE m.type WHEN 1 THEN \"GROUP\" WHEN 0 THEN \"1-ON-1\" END as type, replace(m.text,cast(X'EFBFBC' as text),\"[IMG]\") as text, (SELECT GROUP_CONCAT(\"MediaDomain-\"||substr(a.filename,3)) FROM message_attachment_join ma JOIN attachment a ON ma.attachment_id = a.ROWID WHERE ma.message_id = m.ROWID GROUP BY ma.message_id) as filereflist FROM chat_message_join cm INNER JOIN message m ON cm.message_id = m.ROWID INNER JOIN handle h ON m.handle_id = h.ROWID WHERE m.ROWID = \"16460\" ORDER BY chatgroup, date LIMIT 1;";
+                SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                SQLiteDataReader row = command.ExecuteReader();
+                htmlOutput += "<HTML>\n";
+                htmlOutput += "<HEAD>\n";
+                htmlOutput += "<TITLE>Messages from " + chatGroup + "</TITLE>\n";
+                htmlOutput += "<STYLE TYPE=\"text/css\">\n";
+                htmlOutput = addHTMLHeaders(htmlOutput);
+                htmlOutput += "</STYLE>\n";
+                htmlOutput += "</HEAD>\n";
+                htmlOutput += "<BODY>\n";
+                htmlOutput += "<H1>Messages from " + chatGroup + "</H1>\n";
+                htmlOutput += "<H2>as of " + dbFileDate + "</H2>\n";
+                htmlOutput += "<DIV id=\"messages\">\n";
+                // date, service, direction, id, text, filereflist
+                //htmlOutput += "<TR><TH>DATE</TH><TH>SERVICE</TH><TH>DIRECTION</TH><TH>ID</TH><TH>TEXT</TH><TH>FILEREFLIST</TH></TR>\n";
+                while (row.Read())
+                {
+                    string content = (string)row["text"];
+                    htmlOutput += "<DIV class=\"message-" + row["direction"] + "-" + row["service"] + "\">";
+                    htmlOutput += "<DIV class=\"timestamp\">" + row["date"] + "</DIV>";
+                    if (isGroupMessage && row["direction"].ToString() == "RCVD" ) htmlOutput += "<DIV class=\"sender\">" + row["id"] + "</DIV>";
+                    // replace image placeholders (ï¿¼) with image files 
+                    if (row["filereflist"].ToString().Length > 0)
+                    {
+                        List<string> mediaFileList = row["filereflist"].ToString().Split(',').ToList();
+                        foreach (string mediaFile in mediaFileList) {
+                            // get extension of mediaFile
+                            // do switch statement for replacement string
+                            Regex rgx = new Regex(@"\[MEDIA\]");
+                            content = rgx.Replace(content, "<img src=\"" + dbFileDir + @"\" + getSHAHash(mediaFile) + "\"><!-- "+ mediaFile + " //-->", 1);
+                        }
+                    }
+                    htmlOutput += "<DIV class=\"content\">" + content + "</DIV>\n";
+                    htmlOutput += "</DIV>\n";
+                }
+                htmlOutput += "</DIV>\n";
+                htmlOutput += "</BODY>\n";
+                htmlOutput += "</HTML>\n";
+
+                row.Close();
+                m_dbConnection.Close(); // should only do this once?
+            }
+
+            // regex replacements
+            // REGEX: change phone number format (+12257490000 => (225)749-0000)
+            htmlOutput = Regex.Replace(htmlOutput, @"\+\d(\d{3})(\d{3})(\d{4})", "($1)$2-$3");
+            // REGEX: change date format (2015-01-01 00:00 => 01/01/2015 12:00am)
+            htmlOutput = Regex.Replace(htmlOutput, @"(\d{4})-(\d{2})-(\d{2})\s(\d{2}):(\d{2}):(\d{2})", delegate (Match match)
+            {
+                int hour = int.Parse(match.ToString().Substring(11, 2));
+                string suffix = (hour < 12) ? "am" : "pm";
+                if (hour == 0) hour += 12;
+                else if (hour > 12) hour -= 12;
+                string replace = "$2/$3/$1 " + hour + ":$5" + suffix;
+                return match.Result(replace);
+            });
+
+
+            //htmlOutput = datetimeRegex.Replace(htmlOutput, @"$2/$3/$1 $4:$5 am",datetimeEvaluator);
+
+
+            // do this in separate thread so it won't lock up program
+
+            // output html data
+            File.WriteAllText(htmlFileDialog.FileName,htmlOutput);
+            //System.IO.FileStream fs = (System.IO.FileStream)htmlFileDialog.OpenFile();
+            //fs.Close();
+
+            MessageBox.Show("HTML Export completed!");
+            
+
+        }
+    }
+}
